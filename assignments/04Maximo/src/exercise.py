8ecbdd89f65c0094bdfddbdd8051e0b2c3e47794
shanimucar <88851097+shanimucar@users.noreply.github.com>
2021-08-20 16:12:03 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -12,7 +12,14 @@ def main():
         print(num2)
     elif num3 > num1 and num3 > num2:
         print(num3)
-    
+    elif num1 == num2 and num3 < num1:
+        print(num1)
+    elif num2 == num3 and num1 < num2:
+        print(num2)
+    elif num3 == num1 and num2 < num3:
+        print(num3)
+    elif num1 == num2 and num2 == num3:
+        print(num1)
     
 
 
